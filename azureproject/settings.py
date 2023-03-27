42c6922ba154ad0379685a758b1631e1cc12aa8a
vmagelo <v-gelorminom@microsoft.com>
2023-03-27 17:16:39 +0200

    Get secret key from env variable.

--- azureproject/settings.py
+++ azureproject/settings.py
@@ -1,10 +1,11 @@
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7ppocbnx@w71dcuinn*t^_mzal(t@o01v3fee27g%rg18fc5d@'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # Application definition
 INSTALLED_APPS = [
